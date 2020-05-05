06f0f7cdb968a249419b8e391c8d9622e767708e
AitorZaragoza <aitorzag@gmail.com>
2020-05-05 14:22:47 +0200

    test1

--- Test/simple/ContainerTest.java
+++ Test/simple/ContainerTest.java
@@ -1,5 +1,6 @@
 package simple;
 
+import org.junit.Before;
 import org.junit.Test;
 import simple.FactoryTest.FactoryD1;
 import simple.implementations.ImplementationD1;
@@ -12,19 +13,17 @@ import static org.junit.Assert.assertThat;
 
 public class ContainerTest {
 
+
     @Test
-    public void test1() throws DependencyException {
+    public void TestFactoryOneParam() throws DependencyException {
         Injector injector = new Container();
         injector.registerConstant("I", 42);
         injector.registerFactory("D", new FactoryD1(), "I");
-
 
         InterfaceD d = (InterfaceD) injector.getObject("D");
         assertThat(d, is(instanceOf(ImplementationD1.class)));
         ImplementationD1 d1 = (ImplementationD1) d;
         assertThat(d1.getI(), is(42));
-
-
 
     }
 
@@ -59,6 +58,32 @@ public class ContainerTest {
         assertThat(d12, instanceOf(ImplementationD1.class));
         ImplementationD1 d11 = (ImplementationD1) d12;
         assertThat(d11.getI(), is(42));
+
+    }
+
+    @Test(expected = DependencyException.class)
+    public void DuplicateConstantError() throws DependencyException{
+        Injector injector = new Container();
+        injector.registerConstant("I", 42);
+        injector.registerConstant("I", 94);
+    }
+
+    @Test(expected = DependencyException.class)
+    public void DuplicateFactoryError() throws DependencyException{
+        Injector injector = new Container();
+        injector.registerConstant("I", 42);
+        injector.registerFactory("D", new FactoryD1(), "I");
+        injector.registerFactory("D", new FactoryD1(), "I");
+    }
+
+    @Test(expected = DependencyException.class)
+    public void getNonExistentError() throws DependencyException{
+        Injector injector = new Container();
+        InterfaceD d = (InterfaceD) injector.getObject("D");
+    }
+
+    @Test
+    public void juego() throws DependencyException{
 
 
     }
